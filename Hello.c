08d7971bfc8a07b3bdb895dc488f8616993bf31b
cobeygragg <cgragg2@huskers.unl.edu>
2020-08-24 17:06:54 -0500

    added collaborator name and major

--- Hello.c
+++ Hello.c
@@ -10,7 +10,7 @@
 int main(int argc, char** argv)
 {
     printf("Luke Olson!\n");
-    printf("I am majoring in Computer Science!\n")
+    printf("I am majoring in Computer Science!\n");
     printf("Cobey Gragg!\n");
     printf("I am majoring in computer engineering!\n");
     return 0;
